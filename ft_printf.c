f83346743f42a7e4c50962a9479b526f530c3f72
Ahmet Faruk Basaran <ahbasara@k2m03s07.42kocaeli.com.tr>
2023-01-08 08:31:02 +0300

    use pointer arithmetics & move condition

--- ft_printf.c
+++ ft_printf.c
@@ -58,45 +58,54 @@ int	ft_itoa_base(unsigned long long nbr, int base, char *str, int mod)
 	return (len);
 }
 
-int	ft_check_format(va_list args, char **str)
+int	ft_check_format(va_list args, const char **str, int len)
 {
-	int	len;
-
-	len = 0;
-	if ((**str)++ != '%')
-		len += ft_putchar(str[index]);
-	else if ((**str) == 'c' && (**str)++)
-		len += (1 + ft_putchar(va_arg(args, int)));
-	else if ((**str) == 's' && (**str)++)
-		len += (1 + ft_putstr(va_arg(args, char *)));
-	else if ((**str) == 'u' && (**str)++)
-		len += (1 + ft_itoa_base(va_arg(args, unsigned int), 10, "0123456789", 0));
-	else if ((**str) == 'd' || (**str) == 'i' || (**str) == 'u' && (**str)++)
-		len += (1 + ft_itoa_base(va_arg(args, int), 10, "0123456789", 1))(1 + ;
-	else if ((**str) == 'p' && (**str)++)
-		len += (1 + ft_itoa_base(va_arg(args, unsigned long long), 16,
-				"0123456789abcdef", 2));
-	else if ((**str) == 'x' && (**str)++)
-		len += (1 + ft_itoa_base(va_arg(args, unsigned int), 16,
-				"0123456789abcdef", 0));
-	else if ((**str) == 'X' && (**str)++)
-		len += (1 + ft_itoa_base(va_arg(args, unsigned int), 16,
-				"0123456789ABCDEF", 0));
-	else if ((**str) == '%' && (**str)++)
-		len += (1 + ft_putchar('%'));
+	if (**str != '%')
+		len += ft_putchar(*(*str)++);
+	else if ((*++(*str)) == 'c' && (*str)++)
+		len += ft_putchar(va_arg(args, int));
+	else if ((**str) == 's' && (*str)++)
+		len += ft_putstr(va_arg(args, char *));
+	else if ((**str) == 'u' && (*str)++)
+		len += ft_itoa_base(va_arg(args, unsigned int), 10, "0123456789", 0);
+	else if (((**str) == 'd' || (**str) == 'i' || (**str) == 'u') && (*str)++)
+		len += ft_itoa_base(va_arg(args, int), 10, "0123456789", 1);
+	else if ((**str) == 'p' && (*str)++)
+		len += ft_itoa_base(va_arg(args, unsigned long long), 16,
+				"0123456789abcdef", 2);
+	else if ((**str) == 'x' && (*str)++)
+		len += ft_itoa_base(va_arg(args, unsigned int), 16,
+				"0123456789abcdef", 0);
+	else if ((**str) == 'X' && (*str)++)
+		len += ft_itoa_base(va_arg(args, unsigned int), 16,
+				"0123456789ABCDEF", 0);
+	else if ((**str) == '%' && (*str)++)
+		len += ft_putchar('%');
 	return (len);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	int		index;
 	int		len;
 	va_list	args;
-	
+
 	len = 0;
 	va_start(args, str);
 	while (*str != '\0')
-		len += ft_check_format(args, &str);
+		len += ft_check_format(args, &str, 0);
 	va_end(args);
 	return (len);
 }
+
+
+/* #include <stdio.h>
+int	main(void)
+{
+	//int a;
+	//void	*p = &a;
+	char b;
+
+	b = 'w';
+	printf("%d\n", ft_printf("|my,   |%d| asdds|", 12333));
+	printf("%d", printf("|or,   |%d| asdds|", 12333));
+} */
